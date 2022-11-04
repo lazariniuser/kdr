e966d5c6960e245740481fbe58bd767fc5e14444
lazariniuser <gabriellazarinicontato@gmail.com>
2022-11-04 14:56:11 -0300

    commit novembro

--- logged.php
+++ logged.php
@@ -9,13 +9,23 @@
         <div class="row ">
 
             <div class="col-12 bg-light header-main">
-                <div class="profile-avatar col-2 rounded-circle">
+                <div class="profile-avatar col-2 rounded-circle d-inline">
                 <img src="" class="bg-light w-100 img-fluid">
                 </div>
-                <form class="col-2">
-                    <input class="form-control" type="search" placeholder="Pesquisar interesses"/>
+  
+                <form class="form-control">
+                    <input  type="text" class="border-0" placeholder="Pesquisar interesses"/>
+                    <input type="image" value="">
+                    <script  src="https://cdn.lordicon.com/qjzruarw.js"></script>
+                        <lord-icon
+                        src="https://cdn.lordicon.com/xfftupfv.json"
+                        trigger="hover"
+                        colors="primary:#121331"
+                        state="hover"
+                        style="width:25px;height:25px">
+                        </lord-icon>
                 </form>
-            </div>
+
 
         </div>
     </div>
